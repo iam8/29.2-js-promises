3f7481ef3ec88da57b640f7955c9fe549540b40d
alexm <alex-m@outlook.com>
2023-03-21 18:32:35 -0700

    Add initial code to send GET request to numbers API

--- number_facts.js
+++ number_facts.js
@@ -1,2 +1,12 @@
 // Ioana A Mititean
 // 29.2 - JS Promises
+
+let number_trivia;
+
+$.getJSON("http://numbersapi.com/42?json", (response) => {
+        number_trivia = response;
+        console.log("done", number_trivia)
+    }
+);
+
+console.log("Waiting");
